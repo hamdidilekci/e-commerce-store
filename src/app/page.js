c8bba31bf7f5cd10d9dd0e5c90960c72cbb61ce0
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-11-30 01:45:44 +0300

    remove example from and change css

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
-import StoreForm from "../components/StoreForm";
 import StoreList from "../components/StoreList";
-import Example from "../components/Example";
-
+import StoreForm from "../components/StoreForm";
 import { StoreDataProvider } from "../contexts/StoreContext";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-acikGri p-20">
       <StoreDataProvider>
         <StoreForm />
-        {/* <Example /> */}
         <StoreList />
       </StoreDataProvider>
     </main>
