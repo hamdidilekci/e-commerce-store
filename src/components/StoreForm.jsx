76caa7d177c20b808e40606b634b46b921808575
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-11-30 01:47:56 +0300

    add dataList for suggestions,add header,change css

--- src/components/StoreForm.jsx
+++ src/components/StoreForm.jsx
@@ -3,6 +3,8 @@ import React, { forwardRef } from "react";
 import { Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import { useStoreData } from "../contexts/StoreContext";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { IconButton } from "@mui/material";
 
 const storeSchema = Yup.object().shape({
   storeName: Yup.string().required("Please enter a store name"),
@@ -21,14 +23,8 @@ const storeSchema = Yup.object().shape({
 });
 
 const StoreForm = forwardRef((props, ref) => {
-  const { handleClose, store } = props;
+  const { handleClose, store, mode } = props;
   const { rows, setRows } = useStoreData();
-
-  // form layout
-  const fieldLayoutClassName = "grid grid-cols-1 md:grid-cols-2 mb-6";
-  const fieldClassName = "w-full p-2 border rounded";
-  const labelClassName = "text-sm font-medium leading-6 text-gray-900";
-  const errorClassName = "text-red-500 text-xs mt-1";
 
   // if edit modal open, set initial values as store values
   const initialValues = {
@@ -42,12 +38,34 @@ const StoreForm = forwardRef((props, ref) => {
     description: store?.description || "",
   };
 
+  // form css
+  const layoutClassName = "grid grid-cols-1 md:grid-cols-4 md:m-8 bg-beyaz";
+  const labelClassName =
+    "text-opacity-80 font-medium leading-6 text-gray-900 md:col-span-1";
+  const fieldClassName =
+    "w-full p-2 rounded border-light focus:ring-2 md:col-span-3";
+  const errorClassName = "text-red-500 text-xs mt-1";
+  let outherDivClassName = "max-w-full overflow-x-auto border rounded bg-beyaz";
+
+  // if mode is "modal", make outher div className to medium screen
+  if (mode === "modal") {
+    outherDivClassName = "rounded bg-beyaz";
+  }
+
   return (
-    <div className="container mx-auto bg-white p-8 mt-10 rounded shadow-md md:max-w-2xl">
+    <div className={outherDivClassName}>
+      <div className="m-6 font-medium text-xl text-opacity-80">
+        <IconButton>
+          <KeyboardArrowDownIcon />
+        </IconButton>
+        <span>Store Managament</span>
+        <hr />
+      </div>
       <Formik
         initialValues={initialValues}
         validationSchema={storeSchema}
         onSubmit={(values, { resetForm }) => {
+          console.log("--- form values ---", values);
           // if editing an existing store
           if (props.store) {
             const updatedRows = rows.map((row) =>
@@ -67,33 +85,37 @@ const StoreForm = forwardRef((props, ref) => {
           handleClose();
         }}
       >
-        {({
-          errors,
-          touched,
-          isSubmitting,
-          handleChange,
-          values,
-          setFieldValue,
-        }) => (
+        {({ errors, touched, isSubmitting }) => (
           <Form>
             {/* Store Name */}
-            <div className={fieldLayoutClassName}>
+            <div className="grid grid-cols-1 md:grid-cols-4 md:m-8">
               <label htmlFor="storeName" className={labelClassName}>
-                Store Name
+                <span>Store Name</span>
               </label>
               <Field
                 type="text"
                 id="storeName"
                 name="storeName"
+                list="storeNameOptions"
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="storeNameOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.storeName} key={row.id}>
+                      {row.storeName}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.storeName && touched.storeName && (
                 <div className={errorClassName}>{errors.storeName}</div>
               )}
             </div>
 
             {/* Country */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="country" className={labelClassName}>
                 Country
               </label>
@@ -101,15 +123,26 @@ const StoreForm = forwardRef((props, ref) => {
                 type="text"
                 id="country"
                 name="country"
+                list="countryOptions"
                 className={fieldClassName}
-              ></Field>
+                autoComplete="off"
+              />
+              <datalist id="countryOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.country} key={row.id}>
+                      {row.country}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.country && touched.country && (
                 <div className={errorClassName}>{errors.country}</div>
               )}
             </div>
 
             {/* State */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="state" className={labelClassName}>
                 State
               </label>
@@ -117,15 +150,26 @@ const StoreForm = forwardRef((props, ref) => {
                 type="text"
                 id="state"
                 name="state"
+                list="stateOptions"
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="stateOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.state} key={row.id}>
+                      {row.state}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.state && touched.state && (
                 <div className={errorClassName}>{errors.state}</div>
               )}
             </div>
 
             {/* Address */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="address" className={labelClassName}>
                 Address
               </label>
@@ -133,15 +177,26 @@ const StoreForm = forwardRef((props, ref) => {
                 type="text"
                 id="address"
                 name="address"
+                list="addressOptions"
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="addressOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.address} key={row.id}>
+                      {row.address}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.address && touched.address && (
                 <div className={errorClassName}>{errors.address}</div>
               )}
             </div>
 
             {/* Phone */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="phone" className={labelClassName}>
                 Phone
               </label>
@@ -149,15 +204,26 @@ const StoreForm = forwardRef((props, ref) => {
                 type="text"
                 id="phone"
                 name="phone"
+                list="phoneOptions"
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="phoneOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.phone} key={row.id}>
+                      {row.phone}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.phone && touched.phone && (
                 <div className={errorClassName}>{errors.phone}</div>
               )}
             </div>
 
             {/* Maximum Discount Rate */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="discountRateMax" className={labelClassName}>
                 Maximum Discount Rate
               </label>
@@ -165,15 +231,26 @@ const StoreForm = forwardRef((props, ref) => {
                 type="number"
                 id="discountRateMax"
                 name="discountRateMax"
+                list="discountRateMaxOptions"
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="discountRateMaxOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.discountRateMax} key={row.id}>
+                      {row.discountRateMax}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.discountRateMax && touched.discountRateMax && (
                 <div className={errorClassName}>{errors.discountRateMax}</div>
               )}
             </div>
 
             {/* Maximum Bonus Rate */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="bonusRateMax" className={labelClassName}>
                 Maximum Bonus Rate
               </label>
@@ -181,15 +258,26 @@ const StoreForm = forwardRef((props, ref) => {
                 type="number"
                 id="bonusRateMax"
                 name="bonusRateMax"
+                list="bonusRateMaxOptions"
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="bonusRateMaxOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.bonusRateMax} key={row.id}>
+                      {row.bonusRateMax}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.bonusRateMax && touched.bonusRateMax && (
                 <div className={errorClassName}>{errors.bonusRateMax}</div>
               )}
             </div>
 
             {/* Description */}
-            <div className={fieldLayoutClassName}>
+            <div className={layoutClassName}>
               <label htmlFor="description" className={labelClassName}>
                 Description
               </label>
@@ -197,19 +285,30 @@ const StoreForm = forwardRef((props, ref) => {
                 type="text"
                 id="description"
                 name="description"
+                list="descriptionOptions"
                 rows={3}
                 className={fieldClassName}
+                autoComplete="off"
               />
+              <datalist id="descriptionOptions">
+                {rows?.map((row) => {
+                  return (
+                    <option value={row.description} key={row.id}>
+                      {row.description}
+                    </option>
+                  );
+                })}
+              </datalist>
               {errors.description && touched.description && (
                 <div className={errorClassName}>{errors.description}</div>
               )}
             </div>
-
-            <div className="mt-6 flex items-center justify-end gap-x-6">
+            {/* Buttons */}
+            <div className="pt-4 flex items-center justify-end gap-x-6 bg-acikGri">
               {store && (
                 <button
                   type="button"
-                  className="text-sm font-semibold leading-6 text-gray-900"
+                  className="rounded-md bg-gri px-12 py-2 text-sm font-semibold text-beyaz shadow-sm  hover:text-yesil focus-visible:outline"
                   onClick={handleClose}
                 >
                   Cancel
@@ -217,7 +316,7 @@ const StoreForm = forwardRef((props, ref) => {
               )}
               <button
                 type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-mavi px-12 py-2 text-sm font-semibold text-beyaz shadow-sm hover:text-yesil focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-civit"
                 disabled={isSubmitting}
               >
                 {store ? "Update" : "Save"}
