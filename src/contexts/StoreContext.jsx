53428e830edec85ca13265ae559a74101505ebc3
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-11-30 01:50:11 +0300

    remove console

--- src/contexts/StoreContext.jsx
+++ src/contexts/StoreContext.jsx
@@ -1,4 +1,3 @@
-// store context for keeping StoreForm values and share between components
 "use client";
 import React, { createContext, useContext, useState } from "react";
 
@@ -10,7 +9,7 @@ export const useStoreData = () => {
 
 export const StoreDataProvider = ({ children }) => {
   const [rows, setRows] = useState([]);
-  console.log("context rows", rows);
+
   return (
     <StoreDataContext.Provider
       value={{
